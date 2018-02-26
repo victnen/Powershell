fe38adacb80ce7dc65186300a4ef713914c73bda
Vincent G <34095511+victnen@users.noreply.github.com>
2018-02-26 16:56:38 +0100

    Add files via upload

---
+++ Modules/Stop-VMGuestForce.psm1
@@ -0,0 +1,72 @@
+function Stop-VMGuestForce {
+  <#
+      .Synopsis
+          Stop VM via GuestOS, Then Force  
+      
+      .DESCRIPTION
+          Try to stop GuestOS of VM provide in VMlist. If it stucks, force using VM-Stop
+      
+      .PARAMETER VMList
+          List of VM Name to Stop
+        
+      .EXAMPLE
+          Stop-VMGuestForce -VMList <VM1>,<VM2>     
+                    
+      .OUTPUTS
+          Boolean
+          
+      .Notes
+          Author: Vincent Gitton
+  #>
+  [CmdletBinding()]
+      Param
+      (
+          [Parameter(Mandatory)]
+          [string[]]$VMList
+      ) 
+  Try
+  {   
+    Write-Output "Starting script"
+    Write-Output "$($VMList.count) VM loaded"
+    Foreach ($vm in $VMList)
+    {
+      $TheVM = Get-VM $VM
+      If ($TheVM.PowerState -eq "PoweredOn")
+      {
+         If ($TheVM.Guest.State -eq "Running")
+         {
+            Write-Output "$($TheVM) : Guest Stopping VM"
+            $Task = Stop-VMGuest -VM $TheVM -Confirm:$false
+            $stopwatch =  [system.diagnostics.stopwatch]::StartNew()
+            Do{
+               Start-Sleep -s 20 
+            }Until(((Get-VM $TheVM).powerstate -eq "PoweredOff") -Or ($stopwatch.Elapsed.TotalMinutes -gt 5))     
+            If ((get-vm $VM).PowerState -ne "PoweredOff")
+            {
+               Write-warning "$($TheVM) : Cannot stop GuestOS, Force"
+               $task = Stop-VM -VM $vm -confirm:$false -RunAsync
+               $TaskResult = Wait-Task $task   
+            }
+            $stopwatch.Stop()
+            $stopwatch.Reset()
+         }Else{
+            Write-warning "$($TheVM) : VMtools not running, Stop VM"
+            $task = Stop-VM -VM $vm -confirm:$false -RunAsync
+            $TaskResult = Wait-Task $task      
+         }
+      }Else
+      {
+         Write-Warning "$($TheVM.name) : VM is not running, Skipping." 
+      }     
+   }
+  }Catch
+  {
+    $ErrorMessage = $_.Exception.Message
+    Write-Error $ErrorMessage
+    Write-Error $_.invocationinfo.scriptlinenumber
+    Return $false
+  }Finally
+  {
+   Write-Output "End Of Script" 
+  }
+}
